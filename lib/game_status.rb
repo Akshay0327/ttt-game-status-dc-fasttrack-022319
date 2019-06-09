9a2197e9345f0203b8605e901ed1fd3629acbbde
Akshay Sharma <akshay.sharma09@gmail.com>
2019-06-09 18:03:48 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -5,6 +5,6 @@ end
 
 # Define your WIN_COMBINATIONS constant
 def WIN_COMBINATIONS
-  WIN_COMBINATIONS =[[0,1,2],[3,4,5],[3,4,5],[0,3,6],
+  WIN_COMBINATIONS =[[0,1,2],[3,4,5],[3,4,5],[0,3,6],[1,4,7]
     
     ]
