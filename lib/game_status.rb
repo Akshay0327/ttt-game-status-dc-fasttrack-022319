935b7fbb328f2469f78c75ecf42a38d0a6fb6d54
Akshay Sharma <akshay.sharma09@gmail.com>
2019-06-09 18:34:24 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -8,8 +8,10 @@ end
  WIN_COMBINATIONS =[[0,1,2],[3,4,5],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[6,4,2]]
  
  def won?(board)
-   for WIN_COMBINATIONS.each do |win|
-     if
+   WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
      
    
   end
