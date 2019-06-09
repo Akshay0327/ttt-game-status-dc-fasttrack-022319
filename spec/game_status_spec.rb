fb8d5b9c3c988647b56fb703e49f2d28e86e6d6b
Akshay Sharma <akshay.sharma09@gmail.com>
2019-06-09 18:03:07 +0000

    Automatically backed up by Learn

--- spec/game_status_spec.rb
+++ spec/game_status_spec.rb
@@ -7,7 +7,7 @@ describe "./lib/game_status.rb" do
 
       expect(WIN_COMBINATIONS).to include_array([0,1,2])
       expect(WIN_COMBINATIONS).to include_array([3,4,5])
-      expect(WIN_COMBINATIONS).to include_array([6,7,8])
+      expect(WIN_COMBINATIONS).to include_array([3,4,5])
       expect(WIN_COMBINATIONS).to include_array([0,3,6])
       expect(WIN_COMBINATIONS).to include_array([1,4,7])
       expect(WIN_COMBINATIONS).to include_array([2,5,8])
